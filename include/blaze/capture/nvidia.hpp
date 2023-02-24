c5e263ef46e08d72e929f11efb080d3f953021ed
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2023-02-24 13:30:42 +0200

    add new api

--- include/blaze/capture/nvidia.hpp
+++ include/blaze/capture/nvidia.hpp
@@ -4,25 +4,56 @@
 #include <functional>
 #include <cstdint>
 
-#include <NvFBC.h>
-#include "cuda.h"
+#include <GL/gl.h>
+#include <GL/glx.h>
 
-#include "blaze/capture/nvidia/NvFBCUtils.h"
+#include "NvFBC.h"
+#include "nvEncodeAPI.h"
+#include "NvFBCUtils.h"
+
+namespace blaze {
+
+    enum format : std::uint8_t {
+
+        rgb,
+        rgba,
+        argb,
+        bgra,
+        yuv420p,
+        yuv444p,
+        nv12
+
+    };
+};
 
 namespace blaze::internal {
-
 
     class NvfbcCapture {
 
         protected:
-            void *libNVFBC = nullptr, *libCUDA = nullptr;
+            void *libNVFBC = nullptr, *libEnc = nullptr;
+            void *encoder = nullptr;
+
             NVFBC_API_FUNCTION_LIST pFn;
+            NV_ENCODE_API_FUNCTION_LIST pEncFn;
+
+            GLXContext glxCtx = None;
+            GLXFBConfig glxFBConfig = None;
+
             NVFBCSTATUS fbcStatus;
+            NVENCSTATUS encStatus;
+
             NVFBC_SESSION_HANDLE fbcHandle;
 
-            std::uint8_t *buffer = nullptr;
+            _NVFBC_BUFFER_FORMAT bufferFormat = NVFBC_BUFFER_FORMAT_YUV420P;
+
+            NV_ENC_OUTPUT_PTR outputBuffer = nullptr;
+            NV_ENC_PIC_PARAMS encParams;
+            NV_ENC_REGISTERED_PTR registeredResources[NVFBC_TOGL_TEXTURES_MAX] =
+                {NULL};
+
             std::function<void(const char *, std::int32_t)> errHandler;
-            std::function<void(std::uint8_t *, std::uint64_t)> newFrameHandler;
+            std::function<void(void *, std::uint64_t)> newFrameHandler;
             std::uint16_t refreshRate = 60u;
             NVFBC_SIZE frameSize = {0u, 0u};
 
@@ -33,6 +64,7 @@ namespace blaze::internal {
             NvfbcCapture();
             ~NvfbcCapture();
 
+            void clear();
             void load();
             void setRefreshRate(std::uint16_t fps);
             void setResolution(std::uint16_t width, std::uint16_t height);
@@ -40,8 +72,12 @@ namespace blaze::internal {
             void stopCapture();
             void onErrorCallback(
                 std::function<void(const char *, std::int32_t)> callback);
-            void onNewFrame(
-                std::function<void(std::uint8_t *, std::uint64_t)> callback);
+            void
+                onNewFrame(std::function<void(void *, std::uint64_t)> callback);
+            void setBufferFormat(blaze::format type);
+
+        protected:
+            NVENCSTATUS validateEncodeGUID(void *encoder, GUID encodeGuid);
     };
 
 
