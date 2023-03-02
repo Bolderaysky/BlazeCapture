630fae3c289bce61bd336f32e5db62b5847961e4
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2023-03-02 13:26:28 +0200

    remove audio capture class

--- include/blaze/capture/audio.hpp
+++ include/blaze/capture/audio.hpp
@@ -1,56 +1,11 @@
 #pragma once
 
-#include <cstdint>
-#include <functional>
+#ifdef _WIN32
 
-namespace blaze {
+#include "blaze/capture/win32/audio.hpp"
 
-    enum audioChannel : std::uint8_t {
+#else
 
-        mono = 1u,
-        stereo = 2u
+#include "blaze/capture/linux/audio.hpp"
 
-    };
-
-};
-
-namespace blaze::internal {
-
-    class AudioCapture {
-
-        protected:
-            std::function<void(const char *, std::int32_t)> errHandler;
-            std::function<void(void *, std::uint64_t)> audioStreamHandler;
-
-            std::uint32_t bufferLength = 4096;
-            bool micCapture = true;
-            bool desktopSoundCapture = true;
-
-            std::uint32_t sampleRate = 44100u;
-
-            blaze::audioChannel channelCount = stereo;
-
-
-        public:
-            AudioCapture();
-            ~AudioCapture();
-
-            void setAudioChannel(blaze::audioChannel channelType);
-            void setSampleRate(std::uint32_t rate);
-
-            void load();
-            void startCapture();
-            void stopCapture();
-
-            void setBufferLength(std::uint32_t len);
-
-            void setMicCapturing(bool state);
-            void setDesktopSoundCapturing(bool state);
-
-            void onErrorCallback(
-                std::function<void(const char *, std::int32_t)> callback);
-            void
-                onNewChunk(std::function<void(void *, std::uint64_t)> callback);
-    };
-
-}; // namespace blaze::internal
+#endif
